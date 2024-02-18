224118d909f98f6de407cc72d5b75a321600dfe6
Spaski1 <spaskibratmi@gmail.com>
2024-02-18 23:24:33 +0100

    final version

--- script.js
+++ script.js
@@ -3,22 +3,46 @@ console.log(inputCountry);
 let searchBtn = document.getElementById("searchBtn");
 console.log(searchBtn);
 let resetBtn = document.getElementById("resetBtn");
-let continueUcingEurBtn = document.getElementById("countriesUsingEurBtn");
+let countriesUsingEurBtn = document.getElementById("countriesUsingEurBtn");
 let continueTakingEnglishBtn = document.getElementById("countriesTakingEnglishBtn");
 let macedoniaBtn = document.getElementById("macedoniaBtn");
 let countryContainer = document.getElementById("countryContainer");
-console.log(countryContainer);
-
-
-
+let loader = document.getElementById("loader");
 
 
 async function getCountryData(country) {
+    loaderFunction();
     try {
         let response = await fetch(`https://restcountries.com/v2/name/${country}`);
         let data = await response.json();
         console.log(data);
         writeCountryData(data);
+        loaderFunction2();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function dataForEuro(country) {
+    loaderFunction();
+    try {
+        let response = await fetch(`https://restcountries.com/v2/name/${country}`);
+        let data = await response.json();
+        console.log(data);
+        getCountriesUsingEur(data)
+        loaderFunction2();
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function dataForEnglish(country) {
+    loaderFunction();
+    try {
+        let response = await fetch(`https://restcountries.com/v2/name/${country}`);
+        let data = await response.json();
+        console.log(data);
+        getCountriesTakingEnglish(data)
+        loaderFunction2();
     } catch (error) {
         console.log(error);
     }
@@ -44,23 +68,64 @@ function writeCountryData(data) {
         div.classList.add("country");
         div.innerHTML = `
         <img src="${data[i].flag}">
-        <h2>Name:${data[i].name}</h2>
-        <p>Population:${data[i].population}</p>
-        <p>Capital:${data[i].capital}</p>
-        <p>Area:${data[i].area}</p>
-        
+        <h2>Name: ${data[i].name}</h2>
+        <p>Population: ${data[i].population}</p>
+        <p>Capital: ${data[i].capital}</p>
+        <p>Area: ${data[i].area} km2&sup2</p>
+        `;
+         countryContainer.appendChild(div);
+    }
+}
+
+
+countriesUsingEurBtn.addEventListener("click", () => {
+    countryContainer.innerHTML = "";
+    dataForEuro(inputCountry.value);
+})
+function getCountriesUsingEur(data) {
+   for (let i = 0; i < data.length; i++) {
+    if (data[i].currencies[0].name === "Euro") {
+        let div = document.createElement("div");
+        div.classList.add("country");
+        div.innerHTML = `
+        <img src="${data[i].flag}">
+        <h2>Name: ${data[i].name}</h2>
+        <p>Population: ${data[i].population}</p>
+        <p>Capital: ${data[i].capital}</p>
+        <p>Area: ${data[i].area} km2&sup2</p>
+        <p>currency: ${data[i].currencies[0].name}</p>
         `;
          countryContainer.appendChild(div);
     }
     
-    
-   
+   }
 }
-// List language names and currency names in the table as well ( only names and divided by, EX: Spanish, English )
-// * Add loading image while it is getting the data
-// * Add a filter by name, area, and population in descending order
-// * Add a filter by name, area, and population in ascending order
 
-
-
+continueTakingEnglishBtn.addEventListener("click", () => {
+    countryContainer.innerHTML = "";
+    dataForEnglish(inputCountry.value);
+})
+function getCountriesTakingEnglish(data) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].languages[0].name === "English") {
+            let div = document.createElement("div");
+            div.classList.add("country");
+            div.innerHTML = `
+            <img src="${data[i].flag}">
+            <h2>Name: ${data[i].name}</h2>
+            <p>Population: ${data[i].population}</p>
+            <p>Capital: ${data[i].capital}</p>
+            <p>Area: ${data[i].area} km2&sup2</p>
+            <p>language: ${data[i].languages[0].name}</p>
+            `;
+             countryContainer.appendChild(div);
+        }
+       }
+}
+function loaderFunction() {
+    loader.style.display = "block";
+}
+function loaderFunction2() {
+    loader.style.display = "none";
+}
 
